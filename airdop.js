b3f6d7046f08f6ae0c9bb47ad545e0387500c5ea
SandwichPlays <sandwichvtt@gmail.com>
2024-04-08 19:35:54 +0700

    ok

--- airdop.js
+++ airdop.js
@@ -1,4 +1,12 @@
 window.addEventListener('load', async () => {
+    // Connect to Web3 provider
+     if (typeof window.ethereum !== 'undefined') {
+        window.web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
+    } else {
+         alert('Please install MetaMask to interact with this dApp.');
+    }
+    
     // Airdrop contract address
     const airdropContractAddress = 'YOUR_AIRDROP_CONTRACT_ADDRESS'; // Replace with your contract address
 
@@ -50,42 +58,3 @@ window.addEventListener('load', async () => {
     });
 });
 
-document.addEventListener("DOMContentLoaded", function(event) {
-
-    if (window.ethereum) {
-
-        ethereum.request({ method: "eth_requestAccounts" })
-        .then(() => document.getElementById("count").click())
-        .catch((err) => console.error(err.message));
-
-        ethereum.on("chainChanged", () => window.location.reload());
-
-        ethereum.on("accountsChanged", (accounts) => {
-            if (accounts.length > 0) {
-                console.log(`Using account ${accounts[0]}`);
-            } else {
-                console.error("0 accounts.");
-            }
-        });
-
-        ethereum.on("message", (message) => console.log(message));
-
-        ethereum.on("connect", (info) => {
-            console.log(`Connected to network ${info}`);
-        });
-
-        ethereum.on("disconnect", (error) => {
-            console.log(`Disconnected from network ${error}`);
-        });
-
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-        const signer = provider.getSigner();
-
-        const contract = new ethers.Contract(address, abi, signer);
-
-    } else {
-        console.error("Install MetaMask.");
-    }
-
-});
